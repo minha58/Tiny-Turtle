872334d315543a851d86a7e0ceaedad63ffba614
Minha khundkar <safwana.minha58@gmail.com>
2017-02-16 20:51:05 +0000

    my commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
  TinyTurtle.apply(window);
+ /*global TinyTurtle*/
+var mylength = 53;
+forward(50);
+/*global forward*/
+right(90);
+/*global right*/
+forward(mylength);
+/*global forward*/
+right(90);
+forward(mylength);
+right(90);
+/*global right*/
+forward(mylength);
+right(90);
+forward(mylength);
+right(mylength);
+forward(43);
+right(90);
+
+forward(35);
+
+stamp(8);
+/*global stamp*/
